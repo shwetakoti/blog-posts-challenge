da6afdd80d666e2307101d533e8fff768741e5b5
“Shweta <choti@ravikirans-air.lan>
2018-03-28 20:54:55 -0700

    committing

--- blog-posts-challenge/blogPostsRouter.js
+++ blog-posts-challenge/blogPostsRouter.js
@@ -1,31 +1,46 @@
-
 const express = require('express');
 const router = express.Router();
-const morgan = require('morgan');
+
 const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
-const jsonParser = bodyParser.json();
-const app = express();
+// convenience function for generating lorem text for blog
+// posts we initially add below
+function lorem() {
+  return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ' +
+    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
+    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
+    'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+    'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
+    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+}
 
-// log the http layer
-app.use(morgan('common'));
+// seed some posts so initial GET requests will return something
+BlogPosts.create(
+  '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
+BlogPosts.create(
+  'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
 
-// Adding items to blogposts to start with
-BlogPosts.create('title1','content1','author1');
-BlogPosts.create('title2','content2','author2');
-BlogPosts.create('title3','content3','author3');
-
-// when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/', (req, res) => {
+// add endpoint for GET. It should call `BlogPosts.get()`
+// and return JSON objects of stored blog posts.
+// send back JSON representation of all blog posts
+// on GET requests to root
+router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
-/*app.post('/shopping-list', jsonParser, (req, res) => {
+
+// add endpoint for POST requests, which should cause a new
+// blog post to be added (using `BlogPosts.create()`). It should
+// return a JSON object representing the new post (including
+// the id, which `BlogPosts` will create. This endpoint should
+// send a 400 error if the post doesn't contain
+// `title`, `content`, and `author`
+router.post('/', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
+  const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -34,18 +49,21 @@ app.get('/', (req, res) => {
       return res.status(400).send(message);
     }
   }
-
-  const item = ShoppingList.create(req.body.name, req.body.budget);
+  const item = BlogPosts.create(
+  	req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-app.put('/shopping-list/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id'];
+
+// add endpoint for PUT requests to update blogposts. it should
+// call `BlogPosts.update()` and return the updated post.
+// it should also ensure that the id in the object representing
+// the post matches the id of the path variable, and that the
+// following required fields are in request body: `id`, `title`,
+// `content`, `author`, `publishDate`
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+  	'id', 'title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -54,89 +72,32 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
   if (req.params.id !== req.body.id) {
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
     console.error(message);
     return res.status(400).send(message);
   }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  ShoppingList.update({
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
     id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
   });
   res.status(204).end();
 });
 
-// when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-app.delete('/shopping-list/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+
+// add endpoint for DELETE requests. These requests should
+// have an id as a URL path variable and call
+// `BlogPosts.delete()`
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
   res.status(204).end();
 });
 
-
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-});
-
-app.post('/recipes', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  const item = Recipes.create(req.body.name, req.body.ingredients);
-  res.status(201).json(item);
-});
-
-//PUT method to update the Recipes
-
-app.put('/recipes/:id',jsonParser,(req,res) => {
-   const fields = ['id','name','ingredients']
-   for(let i=0;i<fields.length;i++)
-   {
-      const field = fields[i];
-      if(!(field in req.body))
-      {
-        const message = `Missing \`${field}\` in request body`
-        console.error(message);
-        return res.status(400).send(message);
-      }
- }
-
-   //check if recipe id passed as parameter is same as the one in request object
-
-   if (req.params.id !== req.body.id)
-   {
-     const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-     console.error(message);
-     return res.status(400).send(message);
-   }
-
-   console.log(`Updating recipes list item \`${req.params.id}\``);
-   Recipes.update({
-     id: req.params.id,
-     name: req.body.name,
-     ingredients: req.body.ingredients
-   });
-
-   res.status(204).end();
-})
-
-app.delete('/recipes/:id', (req, res) => {
-  Recipes.delete(req.params.id);
-  console.log(`Deleted recipe \`${req.params.ID}\``);
-  res.status(204).end();
-});*/
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+module.exports = router;
